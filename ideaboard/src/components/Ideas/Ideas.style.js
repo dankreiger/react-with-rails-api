a85d9f99bed9300f1cd29217bab41560c112a66f
Dan Kreiger <dan@dankreiger.com>
2018-11-17 21:37:37 +0100

    feat: one small stupid change for stupid people like me

--- ideaboard/src/components/Ideas/Ideas.style.js
+++ ideaboard/src/components/Ideas/Ideas.style.js
@@ -2,9 +2,9 @@ import styled from 'styled-components';
 import { Container } from 'reactstrap';
 
 export const IdeasContainer = styled(Container)`
-  height: calc(100vh - 80px);
+  /* height: calc(100vh - 80px); */
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-`
+`;
